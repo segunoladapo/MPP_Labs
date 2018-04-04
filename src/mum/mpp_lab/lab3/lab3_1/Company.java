68c8f04737f11523876391ccc5dabe0394648c6d
segun <segxy4show2000@gmail.com>
2018-04-04 07:43:23 -0500

    Print method added to department and company

--- src/mum/mpp_lab/lab3/lab3_1/Company.java
+++ src/mum/mpp_lab/lab3/lab3_1/Company.java
@@ -1,4 +1,21 @@
 package mum.mpp_lab.lab3.lab3_1;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Company {
+    private String name;
+    private List<Department> departments;
+
+    public Company(String name) {
+        this.name = name;
+        this.departments = new ArrayList<>();
+    }
+
+    public void print() {
+        System.out.println("Department ->" + this.name);
+        for (Department department : departments) {
+            department.print();
+        }
+    }
 }
