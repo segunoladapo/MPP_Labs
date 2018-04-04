04c1f777e9deaa29aac1c493f893e8c06e5287d9
segun <segxy4show2000@gmail.com>
2018-04-04 07:43:41 -0500

    Print method added to department and company

--- src/mum/mpp_lab/lab3/lab3_1/Department.java
+++ src/mum/mpp_lab/lab3/lab3_1/Department.java
@@ -1,4 +1,58 @@
 package mum.mpp_lab.lab3.lab3_1;
 
+import javafx.geometry.Pos;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Department {
+    private String name;
+    private String location;
+    private List<Position> positions;
+    private Company company;
+
+    public Department(String name, String location) {
+        this.setName(name);
+        this.setLocation(location);
+        setPositions(new ArrayList<>());
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public List<Position> getPositions() {
+        return positions;
+    }
+
+    public void setPositions(List<Position> positions) {
+        this.positions = positions;
+    }
+
+    public void print(){
+        System.out.println("Department -> " + this.name);
+        for(Position position : positions){
+           // position.print();
+        }
+    }
+
+    public Company getCompany() {
+        return company;
+    }
+
+    public void setCompany(Company company) {
+        this.company = company;
+    }
 }
