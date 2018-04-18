38b0d89ae998f33a913c52c0d8db991da1c4e1f4
segunoladapo <segxy4show2000@gmail.com>
2018-04-18 14:29:55 -0500

    More Lambda expressions added

---
+++ src/mum/mpp_lab/lab6/Debug2.java
@@ -0,0 +1,30 @@
+package mum.mpp_lab.lab6;/*
+    A program to test the Debugger.
+
+*/
+
+import javax.swing.*;
+import java.text.*;
+
+class Debug2 {
+
+    public static void main( String[] args ) {
+
+        int anInt1 = 2;
+        int anInt2;
+        
+        double aDouble1 = 3.8;
+        
+        anInt1 = 3;
+        anInt1 = 4;
+
+        String str1;
+        String str2 = new String("Hello");
+        
+        double radius, area, circumference;
+        
+        System.out.println("anInt1 is  " + anInt1);
+
+
+    }
+}
--- src/mum/mpp_lab/lab8/Main.java
+++ src/mum/mpp_lab/lab8/Main.java
@@ -3,6 +3,7 @@ package mum.mpp_lab.lab8;
 import org.omg.CORBA.CODESET_INCOMPATIBLE;
 
 import java.util.ArrayList;
+import java.util.IntSummaryStatistics;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -18,38 +19,101 @@ public class Main {
                 "29-12-1998", "345-45-345", 34000));
         //No of names that begin with letter B
 
+       // Count the number of last names that begin with the letter  ‘B’.  Print out this number.
         long noOfemployeeWhoseSurnameStartsWithB = employees.stream()
                                                             .filter(employee -> employee.getLastName()
                                                             .startsWith("B"))
                                                             .count();
 
+        //Print out all of the Employee objects whose last name begins with the letter  ‘B’  in sorted order.
         List<Employee> employeeWhoseSurnameStartsWithB = employees.stream()
                                                             .filter(employee -> employee.getLastName()
                                                             .startsWith("B"))
                                                             .sorted()
                                                             .collect(Collectors.toList());
-        List<Employee> changeEmployeeNamesToCapital =  employees.stream()
+
+        //Print out all of the Employee objects whose last name begins with the letter  ‘B’  and
+        // change their first name and last name to be All capital letters.
+                                                            employees.stream()
                                                             .filter(employee -> employee.getLastName()
                                                             .startsWith("B"))
                                                             .map(employee -> {
                                                                 employee.setLastName(employee.getLastName().toUpperCase());
                                                                 employee.setFirstName(employee.getFirstName().toUpperCase());
                                                                 return employee;
-                                                            })
-                                                            .collect(Collectors.toList());
+                                                            }).forEach((e)->
+                                                            {
+                                                                        System.out.println(
+                                                                        e.getLastName() + " " + e.getFirstName());
+                                                            });
 
 
-        List<Employee> allEmployeesWithOnlyLastNameBCapitalized = employees.stream()
+     //Print out all of the Employee objects whose last name begins with the letter  ‘B
+        // and change their first name and last name to be All capital letters.
+                                                                            employees.stream()
                                                                             .map(employee -> {
                                                                                         if(employee.getLastName().startsWith("B")){
                                                                                             employee.setLastName(employee.getLastName().toUpperCase());
                                                                                         }
                                                                                         return employee;
                                                                                 }
-                                                                                ).collect(Collectors.toList());
+                                                                                ).forEach((e)->
+                                                                            {
+                                                                                System.out.println(
+                                                                                        e.getLastName() + " " + e.getFirstName());
+                                                                            });
+        //Use the  Collectors.joining  method to print out All Employee objects.
+        String employeeStr = employees.stream()
+                .map(Employee::toString)
+                .collect(Collectors.joining());
 
-        for(Employee employee : allEmployeesWithOnlyLastNameBCapitalized){
-            System.out.println(employee.getLastName() + " " + employee.getFirstName());
-        }
+
+        //)  Use the  Collectors.joining  method to print out All Employee objects,
+        // and separate    each one with a delimeter of  “---\n---“
+        String employeeStrWithDelimiter = employees.stream()
+                .map(Employee::toString)
+                .collect(Collectors.joining("\n"));
+
+        //Print out all of the Employee objects’ last names, whose last name begins with the letter  ‘I’  in sorted order,
+        // and get rid of all the duplicates.  Print out only the last names.
+
+        employees.stream()
+                .filter(e -> e.getLastName().startsWith("I"))
+                .map(Employee::getLastName)
+                .distinct()
+                .sorted()
+                .forEach(System.out::println);
+
+        //Print out the average of all the salaries.
+        IntSummaryStatistics summary = employees.stream().collect( Collectors.summarizingInt(e ->(int) e.getSalary()));
+        System.out.println(summary.getAverage());
+
+        //Use the  ‘reduce’  method to print out the total salary of all employees.
+        double totalSalary = employees.stream()
+                                    .map(Employee::getSalary)
+                                    .reduce(0.0, (a , b) -> a + b);
+
+        System.out.println(totalSalary);
+
+
+        //Print out only the first names of all the employees.  Use the  ‘map’  method to accomplish this.
+        employees.stream()
+                .map(Employee::getFirstName)
+                .forEach(System.out::println);
+
+
+        //)  Create an infinite stream of even numbers (0, 2, 4, …)
+        // and then, eventually print out only the first 20 even numbers from this stream.
+
+    }
+
+
+    //Implement  a method with the following signature and return type
+    public int countWords(List<String> words, char c, char d, int len){
+        return (int)
+                 words
+                .stream()
+                .filter(word -> word.length() == len && word.contains(String.valueOf(c)) && !word.contains(String.valueOf(d)))
+                .count();
     }
 }
