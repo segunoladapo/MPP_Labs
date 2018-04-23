43de6c21d49a882b4428d7c0f7c46ca41a9851ce
segunoladapo <segxy4show2000@gmail.com>
2018-04-23 14:44:33 -0500

    Java Fx project added

---
+++ src/mum/mpp_lab/jfx/Main.java
@@ -0,0 +1,27 @@
+package mum.mpp_lab.jfx;
+	
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+
+
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			Parent root = FXMLLoader.load(getClass().getResource("Sample.fxml"));
+			Scene scene = new Scene(root,400,400);
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
